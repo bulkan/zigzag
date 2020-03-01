cf488012c8343589b3d7f1f0e3eda5c3e8ef9dda
Bulkan Evcimen <bulkan@gmail.com>
2020-03-01 22:33:18 +1100

    makeArt!

--- src/index.ts
+++ src/index.ts
@@ -12,13 +12,11 @@ new p5((p: p5) => {
   let areaOfSquare;
   let cornerPoints;
   
-  const scaleFactor = 1;
-  let canvas: p5.Graphics | p5;
-
+  let scaleFactor = 1;
   let points: Array<Vector>;
   let triangles: Array<Array<number>>;
 
-  const offset = 20;
+  let offset = 20;
   const squareOffset = 100;
   const MAX_POINTS = 40;
 
@@ -40,7 +38,7 @@ new p5((p: p5) => {
     return [angleA, angleB, angleC];
   }
 
-  function drawLines(innerLineCount, triangle) {
+  function drawLines(canvas, innerLineCount, triangle) {
     const [a, b, c] = triangle;
 
     for (let i = 0; i < innerLineCount; i++) {
@@ -53,7 +51,7 @@ new p5((p: p5) => {
     }
   }
   
-  function drawInnerLines(triangle) {
+  function drawInnerLines(canvas, triangle) {
     const [a, b, c] = triangle;
     const area = areaOfTrig(a, b, c);
 
@@ -61,49 +59,85 @@ new p5((p: p5) => {
     const twoAnglesSame = (new Set(angles.map(a => p.floor(a)))).size !== angles.length;
     const isSkinnyTriangle = angles.some(ang => ang < 7);
     
-    const maxLines = isSkinnyTriangle ? 50 : 200;
+    const maxLines = isSkinnyTriangle ? 50 * scaleFactor : 200;
 
     let innerLineCount = p.floor(p.map(p.log(area * 0.0001 + 1), 0, 12, 1, maxLines, true));
-    let strokeWeight = p.floor(p.map(p.log(area * 0.0001 + 1), 0, 12, 5, 1, true));
+    let strokeWeight = p.floor(p.map(p.log(area * 0.0001 + 1), 0, 12 * scaleFactor, 5 * scaleFactor, 1, true));
     
-    p.strokeWeight(strokeWeight * scaleFactor);
+    p.strokeWeight((strokeWeight));
     
     if (!twoAnglesSame) {
-      drawLines(innerLineCount * scaleFactor, triangle);
+      drawLines(canvas, innerLineCount, triangle);
     } else if (isSkinnyTriangle) {
-      drawLines(innerLineCount * scaleFactor, [c, b, a]); // horizontal
+      drawLines(canvas, innerLineCount, [c, b, a]); // horizontal
     } else {
-      drawLines(innerLineCount * scaleFactor, [c, a, b]); // left
+      drawLines(canvas, innerLineCount, [c, a, b]); // left
     }
   }
 
-  p.setup = () => {
-    const params = p.getURLParams() as ZigZagParams;
-
-    canvas = p;
-
-    if (params.large) {
-      canvas = p.createGraphics(p.windowWidth * scaleFactor + offset, p.windowHeight * scaleFactor + offset);
-    }
-    
-    p.createCanvas(p.windowWidth, p.windowHeight);
-
+  function makeArt(canvas) { 
     canvas.pixelDensity(window.devicePixelRatio || 1);
     canvas.background(background);
+    canvas.noFill();
+    const frameThickness = 10 * scaleFactor;
+
+    canvas.stroke("black");
+    canvas.strokeWeight(frameThickness);
+
+    canvas.translate(canvas.width / 2, canvas.height / 2);
+
+    // center of the square
+    const frameX = -square * scaleFactor / 2;
+    const frameY = -square * scaleFactor / 2;
+
+    // outer rect - frame
+    canvas.rect(frameX - offset, frameY - offset, (square * scaleFactor) + offset * 2, (square  * scaleFactor) + offset * 2);
+
+    // inner rect - frame
+    canvas.rect(frameX - (scaleFactor), frameY - (scaleFactor), (square * scaleFactor), (square  * scaleFactor));
+  
+    canvas.translate(frameX, frameY);
+
+    canvas.stroke("black");
+    canvas.strokeWeight(5 * scaleFactor);
+
+    for (let i = 0; i < triangles.length; i++) {
+      const cell = triangles[i];
+
+      canvas.strokeWeight(3 * scaleFactor);
+      const triangle = cell.map(i => points[i]);
+
+      const [a, b, c] = triangle;
+
+      canvas.stroke("black");
+
+      canvas.triangle(a.x * scaleFactor, a.y * scaleFactor, b.x * scaleFactor, b.y * scaleFactor, c.x * scaleFactor, c.y * scaleFactor);
+
+      drawInnerLines(canvas, triangle);
+    }
+
+    console.log('done drawing');
+  }
+
+  p.setup = () => {    
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    // p.pixelDensity(window.devicePixelRatio || 1);
 
     document.onkeydown = function(e) {
       if (e.metaKey && e.keyCode === 83) {
 
-        if(canvas === p) {
-          canvas.saveCanvas(`zigzag-main-${Date.now()}`, "png");
-        } else {
-          p.save(canvas, `zigzag-${Date.now()}`, "png");
-        }
+        scaleFactor = 2;
+        offset *= scaleFactor;
+
+        const canvas = p.createGraphics(p.windowWidth * scaleFactor + offset, p.windowHeight * scaleFactor + offset);
+        makeArt(canvas);
+        p.save(canvas, `zigzag-${Date.now()}`, "png");
+
         return false;
       }
     };
 
-    square = fitSquares(canvas.width - squareOffset, p.height - squareOffset, 1);
+    square = fitSquares(p.width - squareOffset, p.height - squareOffset, 1);
 
     areaOfSquare = square * square;
 
@@ -131,53 +165,7 @@ new p5((p: p5) => {
     p.noLoop();
   };
 
-  p.draw = () => {
-    canvas.noFill();
-    const frameThickness = 10 * scaleFactor;
-
-    canvas.stroke("black");
-    canvas.strokeWeight(frameThickness);
-
-    canvas.translate(canvas.width / 2, canvas.height / 2);
-
-    // center of the square
-    const frameX = -square * scaleFactor / 2;
-    const frameY = -square * scaleFactor / 2;
-
-    // outer rect - frame
-    canvas.rect(frameX - offset, frameY - offset, (square * scaleFactor) + offset * 2, (square  * scaleFactor) + offset * 2);
-
-    // inner rect - frame
-    canvas.rect(frameX - (scaleFactor), frameY - (scaleFactor), (square * scaleFactor), (square  * scaleFactor));
-  
-    canvas.translate(frameX, frameY);
-
-    canvas.stroke("black");
-    canvas.strokeWeight(5);
-
-    for (let i = 0; i < triangles.length; i++) {
-      const cell = triangles[i];
-
-      canvas.strokeWeight(3);
-      const triangle = cell.map(i => points[i]);
-
-      const [a, b, c] = triangle;
-
-      canvas.stroke("black");
-
-      canvas.triangle(a.x * scaleFactor, a.y * scaleFactor, b.x * scaleFactor, b.y * scaleFactor, c.x * scaleFactor, c.y * scaleFactor);
-
-      drawInnerLines((triangle));
-    }
-
-    // console.log('min', p.min(areas));
-    // console.log('max', p.max(areas));
-    console.log('done drawing');
-
-    if(canvas !== p) {
-      p.save(canvas, `zigzag-${Date.now()}`, "png");
-    }
-  };
+  p.draw = () => makeArt(p);
 });
 
 function fitSquares(width: number, height: number, n) {
